e719c0357ce773ee0be2a8b07f2586da271f4c66
Carlos Vélez <80561943+carlsevels@users.noreply.github.com>
2026-09-12 01:19:35 -0600

    se conecta a supabase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,19 @@ import 'package:get/get.dart';
 
 import 'infrastructure/navigation/navigation.dart';
 import 'infrastructure/navigation/routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   var initialRoute = await Routes.initialRoute;
+  try {
+    await Supabase.initialize(
+      url: 'https://xsbefubqenbsybeupeal.supabase.co',
+      anonKey: 'sb_publishable_UvxcUEUk8PCPKhKkXN1Fuw_JrRXkyri',
+    );
+    print("Conexion exitosa");
+  } catch (e) {
+    print("Error al conctar con el servidor");
+  }
   runApp(Main(initialRoute));
 }
 
@@ -16,9 +26,6 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: initialRoute,
-      getPages: Nav.routes,
-    );
+    return GetMaterialApp(initialRoute: initialRoute, getPages: Nav.routes);
   }
 }
